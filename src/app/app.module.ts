08a6af18e3782eadaa5334010fef6f3d29584004
andrew21-mch <nfonandrew73@gmail.com>
2023-03-16 18:56:20 +0100

    toggle nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCollapseModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavComponent } from './component/nav/nav.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { BodyComponent } from './component/body/body.component';
@@ -35,6 +35,7 @@ import { CreateShopComponent } from './create-shop/create-shop.component';
     FormsModule,
     HttpClientModule,
     NgbModule,
+    NgbCollapseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/nav/nav.component.ts
+++ src/app/component/nav/nav.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  toggleNavbar = true;
   constructor(
     private authService: AuthServiceService
   ) { }
