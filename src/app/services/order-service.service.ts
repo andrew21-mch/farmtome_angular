952badb04108dd89e6e88f8147c2ec3c1dd72ead
andrew21-mch <nfonandrew73@gmail.com>
2023-03-21 19:46:44 +0100

    fix: attempt on cors

--- src/app/services/order-service.service.ts
+++ src/app/services/order-service.service.ts
@@ -3,7 +3,7 @@ import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-const order_route = environment.production ? 'https://agroconnect.herokuapp.com/api/v1/orders' : 'http://localhost:8000/api/v1/orders';
+const order_route = environment.production ? 'https://agroconnect.herokuapp.com/api/v1/orders/' : 'http://localhost:8000/api/v1/orders';
 const httpOptions = {
   headers: new HttpHeaders(
     {
@@ -23,16 +23,18 @@ export class OrderServiceService {
     private readonly http: HttpClient
   ) { }
 
-  placeOrder(formData: FormData): Observable<any>{
-    return this.http.post(order_route, {
-      delivery_method: formData.get('delivery_method'),
-      delivery_address: formData.get('delivery_address'),
-      payment_method: formData.get('payment_method'),
-      product_id: formData.get('product_id'),
-      agro_input_id: formData.get('agro_input_id'),
-      farm_id: formData.get('farm_id'),
-      supplier_shop_id: formData.get('supplier_shop_id'),
-    },
+  placeOrder(formData: FormData): Observable<any> {
+    return this.http.post(
+      order_route,
+      {
+        delivery_method: formData.get('delivery_method'),
+        delivery_address: formData.get('delivery_address'),
+        payment_method: formData.get('payment_method'),
+        product_id: formData.get('product_id'),
+        agro_input_id: formData.get('agro_input_id'),
+        farm_id: formData.get('farm_id'),
+        supplier_shop_id: formData.get('supplier_shop_id'),
+      },
       httpOptions);
 
   }
