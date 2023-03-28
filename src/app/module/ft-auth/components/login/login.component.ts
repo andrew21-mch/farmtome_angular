8ffadd74951c1e5daaa301f3aeaf211524405fed
mofirojean <mofirojean@gmail.com>
2023-03-28 12:37:16 +0100

    implemented the login UI
    completed the login section of the auth  module

---
+++ src/app/module/ft-auth/components/login/login.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserLoginRequest } from 'src/app/models/interface/user.model';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) { 
+  }
+
+  ngOnInit(): void {
+    this.initFormLogin();
+  }
+
+  initFormLogin() {
+    this.loginForm = this.fb.group({
+      email: ["", Validators.required],
+      password: ["", Validators.required]
+    });
+  }
+
+  loginAction() {
+    const userLogin: UserLoginRequest = {
+      email: this.loginForm.get('email')?.value,
+      password: this.loginForm.get('password')?.value
+    }
+    console.log(userLogin)
+  }
+
+}
