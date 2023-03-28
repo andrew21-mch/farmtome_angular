47ebdaef1f12bc9490bea0176cb59d4cc0059b00
mofirojean <mofirojean@gmail.com>
2023-03-28 12:24:13 +0100

    removed auth components from reusable
    change the directory of the login, register and forget password components into the auth module

--- src/app/module/ft-reusable/ft-reusable.module.ts
+++ src/app/module/ft-reusable/ft-reusable.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { FtSharedModule } from '../ft-shared/ft-shared.module';
@@ -14,8 +12,6 @@ import { FtSharedModule } from '../ft-shared/ft-shared.module';
   declarations: [
     NavBarComponent,
     FooterComponent,
-    LoginComponent,
-    RegisterComponent,
     PageNotFoundComponent,
     SideBarComponent
   ],
--- src/app/module/ft-shared/ft-shared.module.ts
+++ src/app/module/ft-shared/ft-shared.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ButtonModule } from 'primeng/button';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    CheckboxModule,
+    ButtonModule
+  ],
+  exports: [
+    InputTextModule,
+    CheckboxModule,
+    ButtonModule
   ]
 })
 export class FtSharedModule { }
