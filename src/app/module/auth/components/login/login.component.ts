3c74564f028fdc42d02ce72e8f9ad8c69c3a16bb
andrew21-mch <nfonandrew73@gmail.com>
2023-04-06 23:08:51 +0100

    fix: login

--- src/app/module/auth/components/login/login.component.ts
+++ src/app/module/auth/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserLoginRequest } from 'src/app/models/interface/user.model';
+import { AuthServiceService } from 'src/app/services/auth-service.service';
 
 @Component({
   selector: 'app-login',
@@ -12,10 +13,16 @@ export class LoginComponent implements OnInit {
 
   loginForm!: FormGroup;
 
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
+
+
   constructor(
     private fb: FormBuilder,
-    private router: Router
-  ) { 
+    private router: Router,
+    private authService: AuthServiceService,
+  ) {
   }
 
   ngOnInit(): void {
@@ -34,7 +41,22 @@ export class LoginComponent implements OnInit {
       email: this.loginForm.get('email')?.value,
       password: this.loginForm.get('password')?.value
     }
-    console.log(userLogin)
+      this.authService.login(userLogin.email, userLogin.password).subscribe({
+        next: data => {
+          this.isSuccessful = true;
+          localStorage.removeItem('token');
+          localStorage.removeItem('user');
+          localStorage.setItem('token', data.token);
+          localStorage.setItem('id', data.user.id);
+          localStorage.setItem('user', JSON.stringify(data.user));
+          this.router.navigateByUrl('/dashboard')
+          this.isSignUpFailed = false;
+        },
+        error: err => {
+          this.errorMessage = err.error.message;
+          this.isSignUpFailed = true;
+        }
+      });
   }
 
   isValidForm() {
